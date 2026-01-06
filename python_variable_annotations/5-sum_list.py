e06c61fbd793786edd9f12d371c21bf265b81b47
meenakshee23 <meenaksheegopaul@gmail.com>
2026-01-06 12:13:12 +0400

    new file

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines a type-annotated fuction sum_list"""
+
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """Return the sum of a list of floats."""
+    return sum(input_list)
