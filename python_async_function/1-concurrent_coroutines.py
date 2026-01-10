c573e1d1a1ab1a0e716989ae6e4482b3abee081b
meenakshee23 <meenaksheegopaul@gmail.com>
2026-01-10 22:06:17 +0400

    new file

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module  writes an async routine called wait_n that takes in 2 int arguments
+"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    """Runs wait_random n times and returns the list of all the delays"""
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    results: List[float] = []
+
+    for task in asyncio.as_completed(tasks):
+        result = await task
+        results.append(result)
+
+    return results
