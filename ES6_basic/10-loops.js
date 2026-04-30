7f218eac92dfafcad6338ee9649e93b3244833a8
meenakshee23 <meenaksheegopaul@gmail.com>
2026-04-30 22:40:47 +0400

    new file

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue(array, appendString) {
+  let idx = 0;
+
+  for (const value of array) {
+    array[idx] = appendString + value;
+    idx++;
+  }
+
+  return array;
+}
